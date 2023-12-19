20fbb2683865d422a6c764bdafaedab939b64cb6
niyasrad <niyasrad@gmail.com>
2023-12-19 16:54:57 +0530

    feat(backend): conditional playlist curation + tags

--- backend/core/schemas/playlist.py
+++ backend/core/schemas/playlist.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel, Field
+from typing import List, Optional
 
 
 class Playlist(BaseModel):
     title: str = Field(..., example="name")
     user_id: str = Field(..., example="1")
+
+
+class PlaylistConditional(BaseModel):
+    title: str = Field(..., example="name")
+    user_id: str = Field(..., example="1")
+    num_songs: Optional[int] = Field(None, example="10")
+    artists: List[str] = Field([], example="['shinee', 'bts']")
+    genre: Optional[List[str]] = Field(None, example="['pop']")
